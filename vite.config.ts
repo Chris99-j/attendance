5d1c799e15323f0a1d3922815aed83876877ca6b
Chris99-j <your.email@example.com>
2025-06-03 20:27:56 +0800

    Save work before build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/attendance/", // <-- important for GitHub Pages repo named 'attendance'
   plugins: [react()],
-})
+});
